f927011ddc2081ce7f53017aaa9402e04b1fdfc8
salima <sahisalima77@gmail.com>
2026-04-02 11:00:46 +0200

    moddleware de verification de token

--- middlewares/authMiddleware.middleware.js
+++ middlewares/authMiddleware.middleware.js
@@ -1,2 +1,26 @@
 import {body,validationResult} from "express-validator"
+import jwt from "jsonwebtoken"
+import User from "../models/User"
 
+
+
+export const verferToken=(req,res,next)=>{
+    try{
+
+        const header=req.header.authorization
+        if(!header){
+            return res.status(401).json({message:"ne trouve pas  token"})
+        }
+
+        const token=header.split(" ")[1]
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded // id w role kayb9aw f req.user
+        next()
+    }catch(err){
+        res.status(401).json({message:"Token invalid"})
+
+
+
+
+    }
+}
--- routes/authRoute.route.js
+++ routes/authRoute.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, registerUser } from "../controllers/authController.controller.js"
+import { verferToken } from "../middlewares/authMiddleware.middleware.js"
 
 const router=express.Router()
 
 router.post("/signUp",registerUser)
-router.post("/login",login)
+router.post("/login",verferToken,login)
 
 
 export default router;
