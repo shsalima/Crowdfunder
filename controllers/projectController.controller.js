9f82fdebe1e3a71610ac0c95bd8530cc8a5e6bc4
salima <sahisalima77@gmail.com>
2026-04-02 16:05:59 +0200

    finilaser les fonctionnalité du project

--- controllers/projectController.controller.js
+++ controllers/projectController.controller.js
@@ -52,3 +52,63 @@ export const getProjectById = async (req, res) => {
     }
 };
 
+
+export const updateProject = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, capital, maxPercentPerInvestor } = req.body;
+
+        const project = await Project.findById(id);
+
+        if (!project) {
+            return res.status(404).json({ message: "Project not found" });
+        }
+
+        // 3and l7a9 ghir owneer
+        if (project.owner.toString() !== req.user.userId) {
+            return res.status(403).json({ message: "Not authorized" });
+        }
+
+        // ghir les project li open
+        if (project.status === "closed") {
+            return res.status(400).json({ message: "Project is closed, cannot be updated" });
+        }
+
+       
+        project.title = title || project.title;
+        project.description = description || project.description;
+        project.capital = capital || project.capital;
+
+        if (maxPercentPerInvestor) {
+            project.maxPercentPerInvestor = Math.min(maxPercentPerInvestor, 10);
+        }
+
+        await project.save();
+
+        res.status(200).json(project);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+export const deleteProject = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const project = await Project.findById(id);
+        if (!project) {
+            return res.status(404).json({ message: "Project not found" });
+        }
+
+       
+        if (project.owner.toString() !== req.user.userId) {
+            return res.status(403).json({ message: "Not authorized" });
+        }
+
+        await project.remove();
+        res.status(200).json({ message: "Project deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
